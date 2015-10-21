c25f33eccdd567e82a9d81e8d0da6601b1b36eee
Daniel Morante <daniel@platform.sh>
2015-10-21 14:00:12 -0400

    Retry

--- modules/mongodb/src/MongoCollectionFactory.php
+++ modules/mongodb/src/MongoCollectionFactory.php
@@ -163,11 +163,11 @@ class MongoCollectionFactory {
   protected function getClient($server) {
     $connection_string = $server['server'];
     if (!isset($this->clients[$connection_string])) {
-	  /*echo "===============\n";
+	  echo "===============\n";
 	  var_dump($connection_string);
 	  echo "===============\n";
 	  var_dump($server);
-  	  echo "===============\n";*/
+  	  echo "===============\n";
       $client = new \MongoClient($connection_string, $server['options']);
       if (!empty($server['read_preference'])) {
         $client->setReadPreference($server['read_preference']);
